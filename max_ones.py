275f1e302e617733317b2e50e8c9f30c06fce51e
Abishekdevil <abishekcholan07@gmail.com>
2025-12-26 15:29:03 +0530

    max consecutive one question solved

---
+++ max_ones.py
@@ -0,0 +1,15 @@
+# Take input from user
+nums = list(map(int, input().split()))
+
+max_count = 0
+current_count = 0
+
+for num in nums:
+    if num == 1:
+        current_count += 1
+        if current_count > max_count:
+            max_count = current_count
+    else:
+        current_count = 0
+
+print( max_count)
